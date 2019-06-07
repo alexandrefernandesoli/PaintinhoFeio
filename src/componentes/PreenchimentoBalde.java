49ce36630048a9869b0d68094244825668e239d3
Guilherme Gonzaga de Andrade <guilhermgonzaga@gmail.com>
2019-06-07 18:36:17 -0400

    Preenchimento por balde finalmente implementado.

--- src/componentes/PreenchimentoBalde.java
+++ src/componentes/PreenchimentoBalde.java
@@ -41,46 +41,52 @@ public class PreenchimentoBalde implements Ferramentas {
 
     private class FloodFill extends Task<Void> {
 
-        private final WritableImage canvasSnapshot;
+        private static final double EPSILON = 0.125;
+        private final WritableImage capturaDoCanvas;
         private final Point ponto;
         private final Color corNova;
 
         private FloodFill(WritableImage capturaDoCanvas, Color corNova, Point ponto) {
-            this.canvasSnapshot = capturaDoCanvas;
+            this.capturaDoCanvas = capturaDoCanvas;
             this.corNova = corNova;
             this.ponto = ponto;
         }
 
+        private boolean coresIguais(Color a, Color b) {
+            return (Math.abs(a.getRed() - b.getRed()) < EPSILON &&
+                    Math.abs(a.getGreen()- b.getGreen()) < EPSILON &&
+                    Math.abs(a.getBlue()- b.getBlue()) < EPSILON);
+        }
+
         @Override
         protected Void call(){
-            PixelReader pixelReader = canvasSnapshot.getPixelReader();
-            PixelWriter pixelWriter = canvasSnapshot.getPixelWriter();
-            Color corAntiga = pixelReader.getColor((int) ponto.getX(), (int) ponto.getY());
+            PixelReader pixelReader = capturaDoCanvas.getPixelReader();
+            PixelWriter pixelWriter = capturaDoCanvas.getPixelWriter();
+            Color corAntiga = pixelReader.getColor(ponto.x, ponto.y);
 
-            if (corAntiga.equals(corNova) /*|| !corNova.equals(corDoPixel)*/)
+            if (coresIguais(corNova, corAntiga))
                 return null;
 
-            Queue<Point> fila = new LinkedList<>();
-            fila.add(ponto);
+            Queue<Point> filaDePontos = new LinkedList<>();
+            filaDePontos.add(ponto);
 
-            while (!fila.isEmpty()) {
+            while (!filaDePontos.isEmpty()) {
+                Point pixel = filaDePontos.remove();
 
-                Point pixel = fila.poll();
-                if (!(pixel.getX() < 0.0f || pixel.getX() >= canvasSnapshot.getWidth() ||
-                        pixel.getY() < 0.0f || pixel.getY() >= canvasSnapshot.getHeight())) {
+                final int[] deslocamentos = {-1, 0, 1};
+                for (int i : deslocamentos) {
+                    for (int j : deslocamentos) {
 
-                    final int[] deslocamentos = {-1, 1};
-                    for (int i : deslocamentos) {
-                        for (int j : deslocamentos) {
+                        Point vizinho = new Point(pixel.x + i, pixel.y + j);
+                        Color corDoVizinho = pixelReader.getColor(vizinho.x, vizinho.y);
 
-                            Point vizinho = new Point((int) pixel.getX() + i, (int) pixel.getY() + j);
-                            Color corDoVizinho = pixelReader.getColor((int) vizinho.getX(), (int) vizinho.getY());
+                        if (0.0 <= vizinho.getX() && vizinho.getX() < capturaDoCanvas.getWidth() &&
+                            0.0 <= vizinho.getY() && vizinho.getY() < capturaDoCanvas.getHeight())
 
-                            if (corDoVizinho.equals(corAntiga)) {
-                                pixelWriter.setColor((int) vizinho.getX(), (int) vizinho.getY(), corNova);
-                                fila.add(vizinho);
+                            if (coresIguais(corDoVizinho, corAntiga)) {
+                                pixelWriter.setColor(vizinho.x, vizinho.y, corNova);
+                                filaDePontos.add(vizinho);
                             }
-                        }
                     }
                 }
             }
